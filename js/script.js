547d6e462619a5f0ba844c827c42793f68ac751f
Cristian Medina <medinacristian@hotmail.com>
2022-06-23 19:07:30 +0200

    Create function that directs to operators

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 // Math operators
 const add = (a, b) => a + b;
-
 const subtract = (a, b) => a - b;
-
 const multiply = (a, b) => a * b;
+const divide = (a, b) => Math.round((a / b) * 100) / 100;
 
-const divide = (a, b) =>  Math.round((a / b) * 100) / 100;
+// Directs to operators
+const operate = (a, b, operator) => {
+  let value;
+
+  switch (operator) {
+    case "add":
+      value = add(a, b);
+      break;
+    case "subtract":
+      value = subtract(a, b);
+      break;
+    case "multiply":
+      value = multiply(a, b);
+      break;
+    case "divide":
+      value = divide(a, b);
+      break;
+  }
+  return value;
+};
